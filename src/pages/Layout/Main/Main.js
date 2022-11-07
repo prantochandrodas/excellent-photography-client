f5007e3123bd290a7c1d8ec4b2c015394ea142f8
pranto <prantochandrodas@gmail.com>
2022-11-07 10:03:20 -0800

    new commit

---
+++ src/pages/Layout/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Home from '../../Home/Home';
+
+const Main = () => {
+    return (
+        <div>
+        
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
