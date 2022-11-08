d1f915f7a760a99206c4ddffe1188d85abb1e68c
pranto <prantochandrodas@gmail.com>
2022-11-08 02:14:50 -0800

    add gmail photo in navbar

--- src/pages/Contexts/AuthProvider.js
+++ src/pages/Contexts/AuthProvider.js
@@ -1,4 +1,4 @@
-import React, { Children, createContext, useState } from 'react';
+import React, { Children, createContext, useEffect, useState } from 'react';
 import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup} from 'firebase/auth'
 import app from '../../firebase/firebase-config';
 
@@ -7,16 +7,28 @@ const auth=getAuth(app);
 export const AuthContext=createContext();
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null);
-
+    const [loading,setLoading]=useState(true);
     //add sign in method
     const createUser=(email,password)=>{
+        setLoading(false);
        return createUserWithEmailAndPassword(auth,email,password);
     }
 
     // add login method
     const login=(email,password)=>{
+        setLoading(false);
         return signInWithEmailAndPassword(auth,email,password);
     }
+
+    useEffect(()=>{
+        const unsubscribe=onAuthStateChanged(auth,(currentUser)=>{
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return()=>{
+            return unsubscribe();
+        }
+    },[])
 
     const signInwithGoogle=(provider)=>{
        return signInWithPopup(auth,provider);
--- src/pages/Layout/Headers/Headers.js
+++ src/pages/Layout/Headers/Headers.js
@@ -3,13 +3,25 @@ import React, { useContext } from 'react';
 import { AuthContext } from "../../Contexts/AuthProvider";
 
 const Headers = () => {
-    const {user}=useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
+    console.log(user);
     const menu = <>
         <li> <Link to='/'>Home</Link> </li>
-        <li> <Link to='/login'>Login</Link></li>
-        <li> <Link to='/signin'>Signin</Link></li>
         <li> <Link to='/'>Blog</Link> </li>
+        {user?.uid ?
+
+            <> <li> <Link to='/logout'>LogOut</Link></li>
+                <div className="avatar">
+                    <div className="w-14 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img alt="" src={user?.photoURL} />
+                    </div>
+                </div></>
+            :
+            <>  <li> <Link to='/login'>Login</Link></li>
+                <li> <Link to='/signin'>Signin</Link></li></>
+        }
+       
 
     </>
     return (
@@ -19,7 +31,7 @@ const Headers = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 pl-3 shadow bg-base-100 rounded-box w-52">
                         {menu}
                     </ul>
                 </div>
@@ -28,7 +40,7 @@ const Headers = () => {
                 </Link>
             </div>
             <div className="navbar-center hidden lg:flex pl-80 ">
-                <ul className="menu menu-horizontal p-0">
+                <ul className="menu menu-horizontal pr-12">
                     {menu}
                 </ul>
             </div>
